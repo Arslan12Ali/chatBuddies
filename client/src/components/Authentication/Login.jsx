4a834021a69265c3547232191445be97ff229c7b
Arslan ali <arslan.ali@emossy.com>
2024-01-29 11:55:43 +0530

    newadd

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("https://chatapplicationforbuddy.onrender.com/", {
+      const response = await fetch("/api/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
