86339fe4e990d0dcdcd0326397add78bdb2e7f10
Arslan ali <arslan.ali@emossy.com>
2024-01-29 11:40:40 +0530

    new code

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatwithcolleagues.onrender.com",
   },
   pingTimeout: 60 * 1000,
 });
